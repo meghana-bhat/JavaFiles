e5efa0ce6aa21abfefbd52b19eeaaa37c9ae3062
meghana <bhatmeghana99@gmail.com>
2022-03-21 15:46:25 +0530

    Basic programs first commit

---
+++ GitCollab/src/com/Basic/SumOfTwoNumbers.java
@@ -0,0 +1,13 @@
+package com.Basic;
+
+public class SumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		int num1=40;
+		int num2=50;
+		
+		System.out.println("Sum="+(num1+num2));
+
+	}
+
+}
